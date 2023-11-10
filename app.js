f8680b886fe396066323cf56a937232058040265
Ivanna _ <ivantina@gmail.com>
2023-11-10 19:42:13 +0100

    commit

---
+++ app.js
@@ -0,0 +1,28 @@
+let num = 45
+let firstName = 'Vanna'
+num = 69
+const isProgrammer = true 
+firstName = 'Baloc'
+
+// alert (firstName)
+// console.log('TEST',firstName)
+// console.log( num+ 10)
+// console.log( num * 10)
+// console.log( num- 10)
+// console.log( num/ 10)
+
+// let num2 = num + 10
+// console.log( num,num2)
+
+// num = num2 - num
+// console.log( num,num2)
+
+// let num3 = (num +15) *3/2 -4
+// console.log(num3) 
+
+// const fullName = firstName+ ' graa'
+// console.log( fullName)
+
+const resultElement = document.getElementById ('result')
+console.log (resultElement.textContent)
+resultElement.textContent = (42-2)/ num 
