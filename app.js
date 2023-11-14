a14249c41ef4e5b956931511da2cca730d9f46d3
Ivanna _ <ivantina@gmail.com>
2023-11-14 19:34:32 +0100

    commit

--- app.js
+++ app.js
@@ -24,16 +24,13 @@ firstName = 'Baloc'
 // console.log( fullName)
 
 const resultElement = document.getElementById ('result')
-console.log (resultElement.textContent)
-// resultElement.textContent = (42-2)/ num 
-
 const input1 = document.getElementById ('input1')
 const input2 = document.getElementById ('input2')
-console.log(input1.value)
-
-
-const sum= Number(input1.value)+ Number(input2.value)
-resultElement.textContent =sum
+console.submitBtn= document.getElementById ('submit')
 
 //console.log(typeof sum)
 
+submitBtn.onclick = function () {
+const sum= Number(input1.value)+ Number(input2.value)
+resultElement.textContent =sum
+}
