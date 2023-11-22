da65630d93585973bc34585d1e95991631927ceb
Ivanna _ <ivantina@gmail.com>
2023-11-22 20:18:58 +0100

    commit

--- Test/notes/app.js
+++ Test/notes/app.js
@@ -15,23 +15,46 @@
 
 /*/
 
-const inputElement = document.getElementById ('title')
-const createBtn = document.getElementById ('create')
-const listElement = document.getElementById ('list')
+const inputElement = document.getElementById("title");
+const createBtn = document.getElementById("create");
+const listElement = document.getElementById("list");
 
-console.log(inputElement.value)
+const notes = ["Block", "Object", 42];
 
-createBtn.onclick=function () {
-    listElement.innerHTML= 
-    `
+function render() {
+  for (let i = 0; i < notes.length; i++) {
+    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[i]));
+  }
+
+  for (let note of notes) {
+    listElement.insertAdjacentHTML("beforebegin", getNoteTemplate(note));
+  }
+
+  listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[1]));
+}
+render();
+
+createBtn.onclick = function () {
+  if (inputElement.value.length === 0) {
+    return;
+  }
+  listElement.insertAdjacentHTML(
+    "beforeend",
+    getNoteTemplate(inputElement.value)
+  );
+  inputElement.value = "";
+};
+
+function getNoteTemplate(title) {
+  return `
         <li
             class= "list-group-item d-flex justify-content-between align-items-center"
             >
-            <span> Name</span>
+            <span> ${title}</span>
             <span>
             <span class="btn btn-small btn-success">&check;</span>
             <span class="btn btn-small btn-danger">&times;</span>
             </span>
-         </li>
-    `
+        </li>
+        `;
 }
