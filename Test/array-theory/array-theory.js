09a561a7a8cd03b9c3afef1503c68aa10a25ff6f
Ivanna _ <ivantina@gmail.com>
2024-01-07 22:29:15 +0100

    COMMIT

--- Test/array-theory/array-theory.js
+++ Test/array-theory/array-theory.js
@@ -1,1 +1,31 @@
-console.log
+const names = ['Tom','Lime','Sam','Lukas']
+
+//names.push('Lime')
+//console.log ('Names:', names,name)
+
+// const name = name.shift()
+// const name = name.pop()
+
+// console.log(names.reverse())
+// const reversed = names.toReversed()
+
+// const letters = ['s','r','g','w','f']
+// console.log(
+//     letters.sort(function (a,b) {
+//     return a-b
+// })
+// )
+// console.log(letters)
+
+// //console.log(names.splice(1,2))
+// console.log(names.toSpliced(1,2))
+// console.log(names)
+
+const greatMan = 'Sam'
+const index = names.indexOf(greatMan)
+console.log (index)
+//console.log (names[index])
+const newNames =names.with (index, 'Sam Willons')
+// console.log (names)
+// console.log (newNames)
+
