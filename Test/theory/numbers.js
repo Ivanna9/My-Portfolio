703fa00123cbdc26fb2184fa5a4af1de68bc923e
Ivanna _ <ivantina@gmail.com>
2024-01-10 19:47:07 +0100

    commit

--- Test/theory/numbers.js
+++ Test/theory/numbers.js
@@ -8,9 +8,9 @@
 // console.log(Number.isFinite(Infinity))
 
 
-const strInt ='33'
-const strFloat ='33.65'
-//1 V
+// const strInt ='33'
+// const strFloat ='33.65'
+// //1 V
 // console.log(Number(strInt))
 // console.log(Number(strFloat))
 
@@ -21,5 +21,43 @@ const strFloat ='33.65'
 // //3V
 // console.log(+strInt, +strFloat)
 
-const fixed = (+(0.5+0.7).toFixed(1))//строчку вернет и с тем количеством нолей которое укажем.
-console.log(parseFloat(fixed))
+// const fixed = (+(0.5+0.7).toFixed(1))//строчку вернет и с тем количеством нолей которое укажем.
+// console.log(parseFloat(fixed))
+
+//BigInt
+// console.log(BigInt(Number.MAX_SAFE_INTEGER)+346346n)
+
+// console.log(32.32n) //error
+// console.log(10n -5)//error
+
+// console.log(parseInt (10n) -5)
+// console.log(10n-BigInt(5))
+// console.log(5n /2) //2 (2,5)  заокруглення
+// console.log(5/2) //2,5
+
+// math
+
+// const myNum = 3.6
+
+//  function getRandomNumber (min,max){
+//     return Math.floor(Math.random() * (max - min +1) +min)
+//  }
+
+//  const num1 = getRandomNumber (10,100)
+//  console.log(num1)
+
+ 
+//  console.log(Math.sqrt(25))
+//  console.log(Math.pow(2,3))
+//  console.log(Math.abs(-21))
+//  console.log(Math.max(3,7,12,54,21,85))
+//  console.log(Math.min(1,6,9,12,4,25))
+//  console.log(Math.floor(myNum))
+//  console.log(Math.ceil(myNUm))
+//  console.log(Math.round(myNum))
+//  console.log(Math.trunc(myNum))
+//  console.log(Math.random())
+
+
+// String
+
