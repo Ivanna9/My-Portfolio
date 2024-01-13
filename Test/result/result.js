6cd9787989521684ebf1e962ce589bb54ef35169
Ivanna _ <ivantina@gmail.com>
2024-01-13 18:16:58 +0100

    commit

---
+++ Test/result/result.js
@@ -0,0 +1,20 @@
+// const car = {
+//     model: 'Tesla',
+//     year: 2022,
+// }
+//  const json = JSON.stringify(car)
+//  const parsed = JSON.parse(json)
+
+//  console.log(json)
+//  console.log(parsed)
+
+async function start() {
+    try {
+        const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await resp.json()
+        console.log(data)
+    }catch(err){
+
+    }
+}
+start()
