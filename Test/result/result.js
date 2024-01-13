ce452b5709f88b0a61d689e9257c6f2172bc278d
Ivanna _ <ivantina@gmail.com>
2024-01-13 19:51:21 +0100

    commit

--- Test/result/result.js
+++ Test/result/result.js
@@ -8,13 +8,52 @@
 //  console.log(json)
 //  console.log(parsed)
 
+const list = document. querySelector('#list')
+const filter = document. querySelector('#filter')
+if (filter) {
+    filter.addEventListener('click', swapper, false);
+  }
+
+let USERS = []
+
+filter.addEventListener('input',(event) => {
+    const value = event.target.value.toLowerCase()
+    const filteredUsers = USERS.filter((user) => 
+    user.name.toLowerCase().includes(value)
+) 
+    render(filteredUsers)
+})
+
 async function start() {
+    list.innerHTML = 'Loading...'
     try {
         const resp = await fetch('https://jsonplaceholder.typicode.com/users')
         const data = await resp.json()
-        console.log(data)
+        setTimeout(() =>{
+            USERS = data
+            render(data)
+        },2000)
+  //      render(data)
+      //  console.log(data)
     }catch(err){
-
+        list.style.color = 'red'
+        list.innerHTML= err.message
     }
 }
+
+function render( users = []){
+    if (user.length === 0){
+        list.innerHTML = 'No matched users!!!'
+    } else {
+        const html= users.map(toHTML).join('')
+        list.innerHTML= html
+    }
+}
+
+function toHTML(user){
+     return`
+    <li class="list-group-item"> ${user.name}</li>
+        `
+}
+
 start()
