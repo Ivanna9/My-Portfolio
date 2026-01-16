eca179611b019a56b4fc713aa65450c01ee96cea
Ivanna <idzuban5@gmail.com>
2026-01-17 00:14:33 +0100

    Theory on arrays

---
+++ 2026/kurs/Praktikum/Notes/array.js
@@ -0,0 +1,16 @@
+// const names = ["Glusio", "Kellay", "Toppo", "Wanuly"];
+// names.push("Glusio");
+// names.unshift("Glusio");
+
+// const firstName = names.shift();
+// const firstName = names.pop();
+// console.log("Names: ", names, firstName);
+
+// console.log(names.reverse());
+// console.log(names.toReversed());
+// console.log(names);
+
+const letters = ["e", "h", "g", "l", "a"];
+// console.log(letters.sort());
+// console.log(letters.pop());
+console.log(letters);
