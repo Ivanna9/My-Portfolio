0fdb04cbf31d0e1b4fa6b4c31e093dbdc90bf2a2
Ivanna <idzuban5@gmail.com>
2026-01-16 23:14:11 +0100

    Interim functionality of the program is complete

--- 2026/kurs/Praktikum/Notes/app.js
+++ 2026/kurs/Praktikum/Notes/app.js
@@ -83,26 +83,16 @@ const notes = [
 ];
 
 function render() {
-  for (let note of notes) {
-    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(note));
+  listElement.innerHTML = "";
+  if (notes.length === 0) {
+    listElement.innerHTML = "<p> No element </p>";
+  }
+  for (let i = 0; i < notes.length; i++) {
+    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[i], i));
   }
 }
 
 render();
-
-function getNoteTemplate(note) {
-  return `
- <li class="list-group-item d-flex justify-content-between align-items-center">
-      <span class="${note.completed ? "text-decoration" : ""}">${
-    note.title
-  }</span>
-      <span class="d-flex gap-2">
-        <span class="btn btn-small btn-success">&check;</span>
-        <span class="btn btn-small btn-danger">&times;</span>
-      </span>
-    </li>
- `;
-}
 
 createBtn.onclick = function () {
   if (inputElement.value.length === 0) {
@@ -112,6 +102,37 @@ createBtn.onclick = function () {
     title: inputElement.value,
     completed: false,
   };
-  listElement.insertAdjacentHTML("beforeend", getNoteTemplate(newNote));
+  notes.push(newNote);
+  render();
   inputElement.value = "";
 };
+
+listElement.onclick = function (event) {
+  if (event.target.dataset.index) {
+    const index = parseInt(event.target.dataset.index);
+    const type = event.target.dataset.type;
+
+    if (type === "toggle") {
+      notes[index].completed = !notes[index].completed;
+    } else if (type === "remove") {
+      notes.splice(index, 1);
+    }
+    render();
+  }
+};
+
+function getNoteTemplate(note, index) {
+  return `
+ <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span class="${note.completed ? "text-decoration-line-through" : ""}">${
+    note.title
+  }</span>
+      <span>
+        <span class="btn btn-small btn-${
+          note.completed ? "warning" : "success"
+        }" data-index="${index}" data-type="toggle">&check;</span>
+        <span class="btn btn-small btn-danger" data-type="remove" data-index ="${index}">&times;</span>
+      </span>
+    </li>
+ `;
+}
