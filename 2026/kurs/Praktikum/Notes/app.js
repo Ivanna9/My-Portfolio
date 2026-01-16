938b4d422bfe3a8c956c3eb5cf141352d884b31d
Ivanna <idzuban5@gmail.com>
2026-01-16 21:06:08 +0100

    Fixed a bug with file paths

--- 2026/kurs/Praktikum/Notes/app.js
+++ 2026/kurs/Praktikum/Notes/app.js
@@ -13,45 +13,40 @@
 // array[0] = "hi";
 // console.log(array);
 
-const inputElement = document.getElementById("title");
-const createBtn = document.getElementById("create");
-const listElement = document.getElementById("list");
+// const inputElement = document.getElementById("title");
+// const createBtn = document.getElementById("create");
+// const listElement = document.getElementById("list");
 
-const notes = ["записать блок про масиви", "розказати теорію обєктів"];
-function render() {
-  // for (let i = 0; i < notes.length; i++) {
-  //   listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[i]));
-  // }
+//erste mal const notes
+// const notes = ["записать блок про масиви", "розказати теорію обєктів"];
+// function render() {
+//    for (let note of notes) {
+//     listElement.insertAdjacentHTML("beforeend", getNoteTemplate(note));
+//   }
+// }
 
-  for (let note of notes) {
-    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(note));
-  }
-}
+// createBtn.onclick = function () {
+//   if (inputElement.value.length === 0) {
+//     return;
+//   }
+//   listElement.insertAdjacentHTML(
+//     "beforeend",
+//     getNoteTemplate(inputElement.value)
+//   );
+//   inputElement.value = "";
+// };
 
-render();
-
-createBtn.onclick = function () {
-  if (inputElement.value.length === 0) {
-    return;
-  }
-  listElement.insertAdjacentHTML(
-    "beforeend",
-    getNoteTemplate(inputElement.value)
-  );
-  inputElement.value = "";
-};
-
-function getNoteTemplate(title) {
-  return `
- <li class="list-group-item d-flex justify-content-between align-items-center">
-      <span class="note-title">${title}</span>
-      <span class="d-flex gap-2">
-        <span class="btn btn-small btn-success">&check;</span>
-        <span class="btn btn-small btn-danger">&times;</span>
-      </span>
-    </li>
- `;
-}
+// function getNoteTemplate(title) {
+//   return `
+//  <li class="list-group-item d-flex justify-content-between align-items-center">
+//       <span class="note-title">${title}</span>
+//       <span class="d-flex gap-2">
+//         <span class="btn btn-small btn-success">&check;</span>
+//         <span class="btn btn-small btn-danger">&times;</span>
+//       </span>
+//     </li>
+//  `;
+// }
 
 /*//object
 
@@ -72,4 +67,51 @@ console.log(person[key]);
 person.getFullName();
 */
 
-const notes = ["записать блок про масиви", "розказати теорію обєктів"];
+const inputElement = document.getElementById("title");
+const createBtn = document.getElementById("create");
+const listElement = document.getElementById("list");
+
+const notes = [
+  {
+    title: "записать блок про масиви",
+    completed: false,
+  },
+  {
+    title: "розказати теорію обєктів",
+    completed: true,
+  },
+];
+
+function render() {
+  for (let note of notes) {
+    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(note));
+  }
+}
+
+render();
+
+function getNoteTemplate(note) {
+  return `
+ <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span class="${note.completed ? "text-decoration" : ""}">${
+    note.title
+  }</span>
+      <span class="d-flex gap-2">
+        <span class="btn btn-small btn-success">&check;</span>
+        <span class="btn btn-small btn-danger">&times;</span>
+      </span>
+    </li>
+ `;
+}
+
+createBtn.onclick = function () {
+  if (inputElement.value.length === 0) {
+    return;
+  }
+  const newNote = {
+    title: inputElement.value,
+    completed: false,
+  };
+  listElement.insertAdjacentHTML("beforeend", getNoteTemplate(newNote));
+  inputElement.value = "";
+};
