a87a7469603bb6010eace997f986ca3b27c9ddba
Ivanna <idzuban5@gmail.com>
2026-01-18 17:49:54 +0200

    Extra theory

--- 2026/kurs/Praktikum/Notes/array.js
+++ 2026/kurs/Praktikum/Notes/array.js
@@ -1,4 +1,4 @@
-// const names = ["Glusio", "Kellay", "Toppo", "Wanuly"];
+const names = ["Glusio", "Kellay", "Toppo", "Wanuly",'Yalisa' ];
 // names.push("Glusio");
 // names.unshift("Glusio");
 
@@ -10,7 +10,31 @@
 // console.log(names.toReversed());
 // console.log(names);
 
-const letters = ["e", "h", "g", "l", "a"];
+// const letters = ["e", "h", "g", "l", "a"];
 // console.log(letters.sort());
 // console.log(letters.pop());
-console.log(letters);
+// console.log(letters.toSorted());
+// console.log(letters)
+
+// // console.log(names.splice(2,4));
+// console.log(names.toSpliced(0,2));
+// console.log(names);
+
+const greateWoman ='Yalisa'
+const index= names.indexOf(greateWoman);
+// console.log(index);
+// console.log(names[index])
+// console.log(names)
+const newNames = names.with(index, "Yalisa Jonnare");
+// console.log(names);
+// console.log(newNames);
+const capitalNames = names.map( function(name, index){
+// const newName= name+ '!'
+// return name.toUpperCase()
+// return name.toLowerCase()
+if (index === 1){ return "Yalisa Jonnare"
+}
+return name
+})
+console.log(capitalNames)
+console.log(names.includes)
