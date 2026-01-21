21ff12fb867d49414af8e45adf458f0440d94a64
Ivanna <idzuban5@gmail.com>
2026-01-21 01:03:10 +0100

    Solving practical problems

--- 2026/kurs/Praktikum/Time/02-strings.js
+++ 2026/kurs/Praktikum/Time/02-strings.js
@@ -5,16 +5,16 @@ const output = `Hello my name is ${name} and my age is ${getAge() > 18 ? "Can" :
 function getAge() {
   return age;
 }
-console.log(output);
-console.log(old);
+// console.log(output);
+// console.log(old);
 
-console.log(name.length);
-console.log(name);
-console.log(name.toUpperCase());
-console.log(name.toLowerCase());
-console.log(name.charAt(4));
-console.log(name.toUpperCase());
-
+// console.log(name.length);
+// console.log(name);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.charAt(4));
+// console.log(name.toUpperCase());
+//............................................................................
 // const array = ["Tom", "Kostya", "Vlad", "Petro", "Slava", "Irina"];
 // console.log(array);
 // // let padded = array.padStart(3, "a"), array.padEnd(3, "a");
@@ -52,7 +52,7 @@ console.log(name.toUpperCase());
 //   }
 // }
 // return (oldArr += myArr);
-// ,,,,,,,,,,,,,,,,,,,,,
+// ,,,,,,,,,,,,,,,,,,,,,старе и почти правильне - пошук по типам
 const info = ["Danil", 1, "Max", 65, "Ira", 29, 32, 16, "Kathy", 71, "Fedia"];
 const copy = [];
 const people = [];
@@ -67,19 +67,69 @@ info.forEach(function (info) {
 console.log("Array is origin:", info);
 console.log("Array of people:", people);
 console.log("Array of number:", copy);
+// ... з типом данних , що зверху
+const info1 = ["Danil", 1, "Max", 65, "Ira", 29, 32, 16, "Kathy", 71, "Fedia"];
+const copy1 = [];
+const people1 = [];
 
+info1.forEach(function (info1) {
+  if (typeof info1 === "string") {
+    people1.push(info1);
+    return true;
+  }
+  copy1.push(info1);
+});
+console.log("Array is origin:", info1);
+console.log("Array of people:", people1);
+console.log("Array of number:", copy1);
+
+//.....................................нове
+// const object = ["___"];
+// const one = ["Tom", "Kostya", "Vlad", "Petro", "Slava", "Irina"];
+// const two = ["___"];
+// one.forEach(function (one) {
+//   const newArr = one.concat(two);
+//   console.log("Array:", newArr);
+//   const ed = two + newArr + two;
+//   console.log(ed);
+//   console.log("Array of new231q23:", newArr);
+// });
+
+// one.forEach(function (one) {
+//   const joined = one.join(two);
+//   console.log("Array:", joined);
+// });
 //.......................
+const one = ["Tom", "Kostya", "Vlad", "Petro", "Slava", "Irina"];
+const two = "___";
+const joined = two + one + two;
+console.log(joined);
+//.........................
 const array = ["Tom", "Kostya", "Vlad", "Petro", "Slava", "Irina"];
-// let result = arr.find(function (item, index, array) {});
-let lengths = ["Bilbo", "Gandalf", "Nazgul"].map((item) => item.length);
+const object = ["___"];
+// const newArr = [];
 
-//.......................
+var newArr = array.join("___");
+//
+// console.log(array.fill("___"));
 
-// arr.forEach(function callback(currentValue, index, array) {
-//     //your iterator
-// }[, thisArg]);
-// ,,,,,,,,,,,,,,,,,,,,,,,,
+array.forEach(function (array) {
+  const newArr = array.concat(object);
+  console.log("Array:", newArr);
+});
+console.log("Array of new:", newArr);
 
-// Давай от тобі задача, зроби масив рядків якихось імен, потім пройдися по ньому і добав спереді і зкінця по 3 символи _
-// Не вкінець і початок масива, а до кожного рядка в середині масива
-// Також почитай про map, filter, reduce
+function add(array, object) {
+  const sum = object + array + object;
+  reduce(sum, add);
+}
+console.log("Array is origin:", array);
+// console.log("Array of object:", object);
+
+function proba() {
+  let arr = ["Tom", "Kostya", "Vlad", "Petro", "Slava", "Irina"];
+  for (let arr of str) {
+    str = arr.join("___");
+  }
+  console.log(str);
+}
