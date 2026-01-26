111f9e8757378e632a3f3f49a1d6f59d4134dbf0
Ivanna <idzuban5@gmail.com>
2026-01-26 18:57:48 +0100

    Creating a countdown program

--- 2026/kurs/Praktikum/Time/time.js
+++ 2026/kurs/Praktikum/Time/time.js
@@ -4,23 +4,48 @@ const fullBtn = document.getElementById("full");
 const dateBtn = document.getElementById("date");
 const timeBtn = document.getElementById("time");
 
-setInterval(update, 1000);
+function bindMode(name) {
+  return function () {
+    mode = name;
+    update();
+  };
+}
+
+fullBtn.onclick = bindMode("full");
+dateBtn.onclick = bindMode("date");
+timeBtn.onclick = bindMode("time");
+
+fullBtn.onclick = function () {
+  mode = "full";
+  update();
+};
+
+dateBtn.onclick = function () {
+  mode = "date";
+  update();
+};
+
+timeBtn.onclick = function () {
+  mode = "time";
+  update();
+};
+
+setInterval(update, 100);
 update();
 function update() {
   output.textContent = format(mode);
 }
-
 //pure function
 function format(formatMode) {
   const now = new Date();
 
   switch (formatMode) {
     case "time":
-      return now.toLocaleTimeString();
+      return now.toLocaleTimeString() + "." + now.getMilliseconds();
     case "date":
       return now.toLocaleDateString();
     case "full":
-      return now.toLocaleDateString() + " " + now.toLocaleDateString();
+      return now.toLocaleDateString() + " " + now.toLocaleTimeString();
     default:
       return now.toLocaleDateString();
   }
