9b779fabcdad2b3438e191eb0e88576ff1f3a4dc
Ivanna <idzuban5@gmail.com>
2026-01-26 18:26:28 +0100

    Display of current time

---
+++ 2026/kurs/Praktikum/Time/time.js
@@ -0,0 +1,27 @@
+let mode = "time";
+const output = document.getElementById("output");
+const fullBtn = document.getElementById("full");
+const dateBtn = document.getElementById("date");
+const timeBtn = document.getElementById("time");
+
+setInterval(update, 1000);
+update();
+function update() {
+  output.textContent = format(mode);
+}
+
+//pure function
+function format(formatMode) {
+  const now = new Date();
+
+  switch (formatMode) {
+    case "time":
+      return now.toLocaleTimeString();
+    case "date":
+      return now.toLocaleDateString();
+    case "full":
+      return now.toLocaleDateString() + " " + now.toLocaleDateString();
+    default:
+      return now.toLocaleDateString();
+  }
+}
