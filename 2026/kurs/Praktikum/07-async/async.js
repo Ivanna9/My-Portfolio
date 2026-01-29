9b55e63346e495aa6d797afbe2ce2647da37e9f1
Ivanna <idzuban5@gmail.com>
2026-01-29 13:30:55 +0100

    continuation of the theory on async

--- 2026/kurs/Praktikum/07-async/async.js
+++ 2026/kurs/Praktikum/07-async/async.js
@@ -1,9 +1,51 @@
-const timeout = setTimeout(() => {
-  console.log("after 2 seconds");
-}, 2000);
+// const timeout = setTimeout(() => {
+//   console.log("after 2 seconds");
+// }, 2000);
 
-clearTimeout(timeout);
+// clearTimeout(timeout);
 
-setTimeout(() => {
-  console.log("after3 seconds");
-}, 3000);
+// setTimeout(() => {
+//   console.log("after3 seconds");
+// }, 3000);
+
+// setInterval(() => {
+//   console.log("tick", ++count);
+// }, 1000);
+
+// function delay(callback, time = 1000) {
+//   setTimeout(callback, time);
+// }
+// delay(() => {
+//   console.log("timeout");
+// }, 2000);
+
+const delay = (time = 1000) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+  return promise;
+};
+
+delay(2500).then(() => {
+  console.log("timeout");
+});
+
+const delay1 = (time = 1000) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([1, 3, 6, 2]);
+    }, time);
+  });
+  return promise;
+};
+
+delay1(2500)
+  .then((data) => {
+    console.log("timeout", data);
+    return data.map((x) => x ** 2);
+  })
+  .then((data) => {
+    console.log(data);
+  });
