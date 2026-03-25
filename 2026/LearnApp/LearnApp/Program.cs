e617ff81853d624daddeb9cf8eed1a607a4f3253
Ivanna <idzuban5@gmail.com>
2026-03-25 16:37:59 +0100

    Test C#
    Добавлен пример работы с файлами и обновлены сборки
    
    В Program.cs реализовано создание и чтение файла test.txt. Добавлен сам файл test.txt. Пересобраны и обновлены бинарные файлы проекта. Обновлены служебные и конфигурационные файлы среды разработки Visual Studio.

--- 2026/LearnApp/LearnApp/Program.cs
+++ 2026/LearnApp/LearnApp/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World again!");
+
+string path = "test.txt";
+
+File.WriteAllText(path, "This is a test file.");
+string content = File.ReadAllText(path);
+Console.WriteLine(content);
