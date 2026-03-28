ab2a90968cec0645108aabe65af386dcedd1f5fe
Ivanna <idzuban5@gmail.com>
2026-03-28 16:56:28 +0100

    reading text from a file

--- 2026/LearnApp/LearnApp/Program.cs
+++ 2026/LearnApp/LearnApp/Program.cs
@@ -1,7 +1,10 @@
 ﻿Console.WriteLine("Hello, World again!");
 
 string path = "test.txt";
-
-File.WriteAllText(path, "This is a test file.");
-string content = File.ReadAllText(path);
-Console.WriteLine(content);
+string[] lines = { "Jova", "Eva", "Pawol" };
+File.WriteAllLines(path, lines);
+string[] fromFile = File.ReadAllLines(path);
+foreach (string line in fromFile)
+{
+    Console.WriteLine(line);
+}
