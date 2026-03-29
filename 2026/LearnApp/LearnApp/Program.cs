f9f7e806d87b6a5ce0ee6b4612021ea21b975175
Ivanna <idzuban5@gmail.com>
2026-03-29 23:49:26 +0200

    Convert the modified list back to JSON

--- 2026/LearnApp/LearnApp/Program.cs
+++ 2026/LearnApp/LearnApp/Program.cs
@@ -1,22 +1,35 @@
 ﻿using System.Text.Json;
+string path = "users.json";
+
 List<User> users = new List<User>
-{ new User{Name="Falio Doe", City = "New York", Age=26},
-    new User{Name="Jane Smith", City = "los Angeles", Age=45},
-    new User{Name="John Liff", City = "Paris", Age=35},
+{
+    new User{Name="Falio ", City = "New York", Age=26},
+    new User{Name="Jane ", City = "los Angeles", Age=45},
+    new User{Name="John ", City = "Paris", Age=35},
 };
 
 string json = JsonSerializer.Serialize(users);
-Console.WriteLine(json);
+File.WriteAllText(path,json);
 
-File.WriteAllText("users.json", json);
-string fromFile = File.ReadAllText("users.json");
-
+string fromFile = File.ReadAllText(path);
 List<User>? loaded = JsonSerializer.Deserialize<List<User>>(fromFile);
-foreach(User user in loaded)
+
+foreach (User user in loaded)
 {
-Console.WriteLine($"{user.Name} - {user.City} - {user.Age}");
+    user.Age += 1;
 }
 
+string updatedJson = JsonSerializer.Serialize(loaded);
+File.WriteAllText(path, updatedJson);
+
+string final = File.ReadAllText(path);
+
+List<User>? result = JsonSerializer.Deserialize<List<User>>(final);
+
+foreach (User user in result)
+{
+    Console.WriteLine($"{user.Name} - {user.City} - {user.Age}");
+}
 
 class User
 {
