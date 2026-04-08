ce1b88fca5c39aa4d5918a4c5d9561c56219b718
Ivanna <idzuban5@gmail.com>
2026-04-08 09:38:52 +0200

    Type exercises

---
+++ 2026/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            int j = 6;
+            Console.WriteLine((a+b)*j);
+            cw
+            int o = ((a + b) * j);
+            Console.WriteLine(o);
+        }
+    }
+}
