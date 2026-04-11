1e6039ba3f1fb54309bf81d13ef71d26ed69f2fc
Ivanna <idzuban5@gmail.com>
2026-04-11 22:46:29 +0200

    double conversion

--- 2026/LearnApp/LearnApp/Program.cs
+++ 2026/LearnApp/LearnApp/Program.cs
@@ -1,28 +1,12 @@
 ﻿using System.Runtime.InteropServices;
+using System.Globalization;
 
-string data;
+string str1 = "1.7";
+NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+{
+    NumberDecimalSeparator = ".",
+};
 
-data = "5";
-string str;
-str = "1";
+double t= Convert.ToDouble(str1, numberFormatInfo);
 
-int a = Convert.ToInt32(str);
-int b = Convert.ToInt32(data);
-
-//Console.WriteLine(b + a);
-
-string str1;
- int num1, num2;
-
-Console.WriteLine("Enter first number:");
- str1 = Console.ReadLine();
-num1= Convert.ToInt32(str1);
-
-Console.WriteLine("Enter second number:");
-str1 = Console.ReadLine();
-num2 = Convert.ToInt32(str1);
-
-
-int sum = num1 + num2;
-Console.WriteLine("The sum of " + num1 + " and " + num2 + " is: " + sum);
 
