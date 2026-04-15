6cedfcc91bae5a13338a483df5baff00c6bc9901
Ivanna <idzuban5@gmail.com>
2026-04-15 21:05:20 +0200

    Added a classic structure to the old file

---
+++ 2026/LearnApp/LearnApp/§06.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Lesson06
+{
+    static void Main()
+    {
+        Console.Write("name ");
+        string? name = Console.ReadLine();
+
+        Console.Write("What is your age? ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("What is your favorite color? ");
+        string? color = Console.ReadLine();
+
+        Console.Write("tall or short? ");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Your name is {name}, you are {age} years old, your favorite color is  {color}, and you are {height} tall.");
+    }
+}
