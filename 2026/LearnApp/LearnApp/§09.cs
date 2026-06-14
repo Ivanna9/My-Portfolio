6f8d4a1008dad5e13f77abf1603fc450e28bda5d
Ivanna <idzuban5@gmail.com>
2026-06-14 14:54:55 +0200

    the order of arithmetic operations

---
+++ 2026/LearnApp/LearnApp/§09.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System;
+
+namespace Lessons
+{
+    class Lesson09
+    {
+        static void Main()
+        {
+            int result1 = 2+2*4;
+            int result2 = 7 * (2 * 4)+(2 * 4);
+            int result3 = 42 * (65 / 4);
+            int result4 = 3 / 4 / 4;
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.WriteLine(result4);
+        }
+    }
+}
+
